c6b374982151c476ada37df675a80aa7d0f80ba5
Dylan Wilson <dylan@craftworkgames.com>
2015-04-11 21:37:53 +1000

    added a dummy unit test

---
+++ Eagle.Server.Framework.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eagle.Server.Framework.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
