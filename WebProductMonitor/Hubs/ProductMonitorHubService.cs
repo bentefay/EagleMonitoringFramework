14fd73c0b6d778114fa3625ca47a3c0b4c9edf66
Ben Tefay <btefay@gmail.com>
2015-04-12 11:47:33 +1000

    Added forgotten files (sorry!)

---
+++ WebProductMonitor/Hubs/ProductMonitorHubService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.SignalR;
+using ProductMonitor.Framework;
+
+namespace WebProductMonitor.Hubs
+{
+    public class ProductMonitorHubService
+    {
+        private ProductMonitorHubService()
+        {
+            Checks = new ICheckDisplay[0];
+        }
+
+        private readonly Lazy<dynamic> _clients = new Lazy<dynamic>(() => GlobalHost.ConnectionManager.GetHubContext<ProductMonitorHub>().Clients.All); 
+
+        public static readonly ProductMonitorHubService Instance = new ProductMonitorHubService();
+
+        public IReadOnlyList<ICheckDisplay> Checks { get; private set; }
+
+        public void UpdateChecks(IReadOnlyList<ICheckDisplay> checks)
+        {
+            Checks = checks;
+            _clients.Value.UpdateChecks(checks.Select(check => new CheckDisplayDto(check)));
+        }
+
+        public void UpdateCheck(ICheckDisplay check)
+        {
+            _clients.Value.UpdateChecks(new[] { new CheckDisplayDto(check) });
+        }
+    }
+}
