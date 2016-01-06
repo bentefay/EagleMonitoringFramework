0a80817da89922c6afc14f8681fc5a168653aa92
Ben Tefay <btefay@gmail.com>
2016-01-06 10:05:07 +1000

    Setting up gulp file

---
+++ EagleMonitoring/EagleMonitoring.Ui.Web/Client/build.config.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+
+    webPack: {
+        entry: './app/main.tsx',
+        output: {
+            filename: 'app-bundle.js'
+        },
+        resolve: {
+            extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+        },
+        module: {
+            loaders: [
+                { test: /\.ts$/, loader: 'ts-loader' }
+            ]
+        }
+    }
+}
